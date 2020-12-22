627300e8e410f684e0b272804c5f77b31b15b092
Sam Cheung <sacheung@kratos0.inf.ethz.ch>
2020-12-22 03:25:24 +0100

    first commit

---
+++ ecoli.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+#BATCH --job-name=test
+#SBATCH --output=ecoli_slurm.txt
+#SBATCH --cpus-per-task=16
+#SBATCH --ntasks=1
+
+#SBATCH --mem=5G
+srun /home/sacheung/zsim/build/opt/zsim m3d_example.cfg
