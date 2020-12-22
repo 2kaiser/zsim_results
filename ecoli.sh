90ba3bf5127f4f1b3ba884bcbda87d1b407884eb
2kaiser <skcheun2@illinois.edu>
2020-12-22 05:28:08 +0100

    adding stuff

--- ecoli.sh
+++ ecoli.sh
@@ -1,8 +1,10 @@
 #!/bin/bash 
 #BATCH --job-name=test
-#SBATCH --output=ecoli_slurm.txt
+#SBATCH --output=ecoli_slurm_baseline.txt
 #SBATCH --cpus-per-task=16
 #SBATCH --ntasks=1
 
 #SBATCH --mem=5G
+srun /home/sacheung/zsim/build/opt/zsim 2d_example.cfg
+srun /home/sacheung/zsim/build/opt/zsim 3d_example.cfg
 srun /home/sacheung/zsim/build/opt/zsim m3d_example.cfg
