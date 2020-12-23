726da4e1343e6d5d8f97b8032c4d70a969df2d29
2kaiser <skcheun2@illinois.edu>
2020-12-23 21:52:01 +0100

    ecoli

---
+++ ecoli/ecoli_collecting_minimizers.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+#BATCH --job-name=test
+#SBATCH --output=ecoli_slurm_collecting_minimizers.txt
+#SBATCH --cpus-per-task=16
+#SBATCH --ntasks=1
+
+#SBATCH --mem=5G
+srun /home/sacheung/zsim/build/opt/zsim 2d_collecting_minimizers.cfg
+srun /home/sacheung/zsim/build/opt/zsim 3d_collecting_minimizers.cfg
+srun /home/sacheung/zsim/build/opt/zsim m3d_collecting_minimizers.cfg
