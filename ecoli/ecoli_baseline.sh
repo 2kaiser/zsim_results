942d71eaeb7a3d69047cf3c75e042823ec27e115
2kaiser <skcheun2@illinois.edu>
2020-12-24 00:41:04 +0100

    ecoli

--- ecoli/ecoli_baseline.sh
+++ ecoli/ecoli_baseline.sh
@@ -1,10 +1,5 @@
 #!/bin/bash 
-#BATCH --job-name=test
-#SBATCH --output=ecoli_slurm_baseline.txt
-#SBATCH --cpus-per-task=16
-#SBATCH --ntasks=1
 
-#SBATCH --mem=5G
-srun /home/sacheung/zsim/build/opt/zsim 2d_baseline.cfg
-srun /home/sacheung/zsim/build/opt/zsim 3d_baseline.cfg
-srun /home/sacheung/zsim/build/opt/zsim m3d_baseline.cfg
+sbatch ecoli_2d_baseline.sh
+sbatch ecoli_3d_baseline.sh
+sbatch ecoli_m3d_baseline.sh
