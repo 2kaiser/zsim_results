06cadf852db359cdad82c167f6a10962331adb52
2kaiser <skcheun2@illinois.edu>
2021-02-06 20:22:25 +0100

    hi

--- human/cache_studies/limit_cycles_tests/human_baseline_UL_long_seed_align_hooks/m3d_64.sh
+++ human/cache_studies/limit_cycles_tests/human_baseline_UL_long_seed_align_hooks/m3d_64.sh
@@ -4,7 +4,7 @@
 
 #SBATCH --ntasks=1
 
-#SBATCH --mem=15G
+#SBATCH --mem=20G
 #srun /home/sacheung/zsim/build/opt/zsim 2d_baseline.cfg
 #srun /home/sacheung/zsim/build/opt/zsim 3d_baseline.cfg
 srun /home/sacheung/zsim/build/opt/zsim m3d_64.cfg
