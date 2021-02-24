52b0e2e32cc51543fe1ee7f3633046ada768dc79
2kaiser <skcheun2@illinois.edu>
2021-02-24 05:22:35 +0100

    hi:

--- human/cache_studies/limit_cycles_tests/human_baseline/2d_64.sh
+++ human/cache_studies/limit_cycles_tests/human_baseline/2d_64.sh
@@ -4,7 +4,7 @@
 
 #SBATCH --ntasks=1
 
-#SBATCH --mem=15G
+#SBATCH --mem=17G
 srun /home/sacheung/zsim/build/opt/zsim 2d_64.cfg
 #srun /home/sacheung/zsim/build/opt/zsim 3d_baseline.cfg
 #srun /home/sacheung/zsim/build/opt/zsim m3d_baseline.cfg
